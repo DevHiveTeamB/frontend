3b7ebab34fe6f69634ed32847aa75049acd90a95
heeyeon <dlgmldus3289@naver.com>
2023-07-28 16:07:56 +0900

    install router

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <div>HelloWorld</div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-
-export default {
-  name: 'App',}
-</script>
-
 <style>
-  body, html {
-    width: 800px;
-    height: 300px;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  #app {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-  }
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
