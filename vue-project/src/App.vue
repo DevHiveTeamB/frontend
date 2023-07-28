0e7cffc833fe2a588c8f77c7c05eb9a174599bdb
Kimwonbin0921 <pransis1@naver.com>
2023-07-28 15:16:04 +0900

    23.07.23 Upload - DevBeen

---
+++ vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>HelloWorld</div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',}
+</script>
+
+<style>
+  body, html {
+    width: 800px;
+    height: 300px;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+
+  #app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+</style>
