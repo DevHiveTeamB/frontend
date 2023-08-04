d5fe0bd16a1a04b284d7b95a28f2e5f1a00479c2
heeyeon <dlgmldus3289@naver.com>
2023-08-04 17:33:36 +0900

    20230804

---
+++ vue-project/src/components/CategoryModal.vue
@@ -0,0 +1,11 @@
+<template>
+  <!-- 카테고리 모달 구현해주세요 
+  디자인하고 기능구현해야될듯..ㅎ-->
+  <div></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- vue-project/src/views/PostPage.vue
+++ vue-project/src/views/PostPage.vue
@@ -1,9 +1,82 @@
 <template>
-  <div>게시글작성페이지</div>
+  <div>
+    <button id="complete" checkForm>완료</button>
+    <div class="content">
+      <!-- 사진 선택하고 화면에 나옴
+      슬라이드로 넘기는거 구현해주세요
+      최대 5장 선택가능하도록 해뒀음 -->
+      <div id="fileContainer">
+        <input type="file" @change="handleFileChange" />
+        <br />
+        <img
+          v-if="selectedImage"
+          :src="selectedImage"
+          alt="선택된 사진"
+          style="max-width: 400px; max-height: 400px"
+        />
+      </div>
+      <div v-if="selectedImages.length > 0">
+        <img
+          v-for="(image, index) in selectedImages"
+          :key="index"
+          :src="image"
+          alt="선택된 사진"
+          class="thumbnail"
+        />
+      </div>
+      <input v-model="title" type="text" placeholder="제목을 입력해주세요" /><br />
+      <input v-model="content" type="text" placeholder="책 소개를 입력해주세요" /><br />
+      <div @click="modalOpen = true">카테고리입력</div>
+      <CategoryModal v-if="modalOpen" />
+      <input v-model="price" type="number" placeholder="가격을 입력해주세요" /><br />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import CategoryModal from '../components/CategoryModal.vue'
+export default {
+  methods: {
+    handleFileChange(event) {
+      const file = event.target.files[0]
+      if (file && this.selectedImages.length < 5) {
+        this.selectedImages.push(URL.createObjectURL(file))
+      }
+    },
+    checkForm() {
+      if (
+        this.title == '' ||
+        this.selectedImage == '' ||
+        this.content == '' ||
+        this.category == '' ||
+        this.price == ''
+      ) {
+        alert('빈칸없이 작성해주세요')
+      }
+    }
+  },
+  data() {
+    return {
+      selectedImage: null,
+      selectedImages: [],
+      title: '',
+      content: '',
+      category: '',
+      price: '',
+      modalOpen: false
+    }
+  },
+  components: {
+    CategoryModal
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.thumbnail {
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  margin: 5px;
+}
+</style>
