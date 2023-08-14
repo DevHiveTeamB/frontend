7dd945de94df4bd3b5018364bbec1d96a7ce916f
Won Chan Lee <twoone14@tukorea.ac.kr>
2023-08-14 17:17:45 +0900

    메인 페이지 퍼블리싱

--- vue-project/src/components/NavBar.vue
+++ vue-project/src/components/NavBar.vue
@@ -2,16 +2,16 @@
   <!-- NAVBAR -->
   <div id="navbar">
     <div class="container">
-      <div id="home" @click="this.$router.push('/')">
+      <div id="home" class="item" @click="this.$router.push('/')">
         <img src="../assets/home/icon_home_disable.svg" />
       </div>
-      <div id="community">
+      <div id="community" class="item">
         <img src="../assets/community/icon_commu_disable.svg" />
       </div>
-      <div id="chat">
+      <div id="chat" class="item">
         <img src="../assets/chat/icon_chat_disable.svg" />
       </div>
-      <div id="mypage" @click="this.$router.push('login')">
+      <div id="mypage" class="item" @click="this.$router.push('login')">
         <img src="../assets/mypage/icon_mypage_disable.svg" />
       </div>
     </div>
@@ -41,9 +41,17 @@ export default {
   width: 360px;
   height: 60px;
   display: flex;
-  justify-content: space-around;
   align-items: center;
   background: #fff;
   border: 2px solid #316464;
+}
+
+.item {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 1;
 }
 </style>
