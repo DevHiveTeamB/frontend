0551f5cd37147ed8420c67d411e9c478fb58b6d2
Won Chan Lee <twoone14@tukorea.ac.kr>
2023-08-16 08:45:27 +0900

    axios baseURL 설정! (https://port-0-springbackend-1b5xkk2fldlaigtw.gksl2.cloudtype.app/api)으로

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+//axios import
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -9,3 +11,7 @@ app.component('icon-base', IconBase)
 app.use(router)
 app.use(store) // Vuex store를 사용함
 app.mount('#app')
+
+export default axios.create({
+  baseURL: 'https://port-0-springbackend-1b5xkk2fldlaigtw.gksl2.cloudtype.app/api'
+})
