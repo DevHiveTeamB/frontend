9ef4d44431cdc633a74ad7b9307054401f0e05e4
Won Chan Lee <twoone14@tukorea.ac.kr>
2023-08-28 13:53:38 +0900

    LoadingSpinner 컴포넌트 추가

---
+++ vue-project/src/components/LoadingSpinner.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="!this.isLoading" class="loading-spinner">
+    <div class="spinner"></div>
+  </div>
+</template>
+<script>
+import { mapGetters,mapMutations } from 'vuex';
+export default {
+computed: {
+  ...mapGetters(['isLoading'])
+},
+data() {
+  return {
+  };
+},
+methods: {
+  ...mapMutations(['setIsLoading']),
+}
+};
+</script>
+<style>
+.loading-spinner {
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100%;
+width: 100%;
+}
+
+.spinner {
+border: 4px solid rgba(0, 0, 0, 0.1);
+border-left: 4px solid #316464;
+border-radius: 50%;
+width: 40px;
+height: 40px;
+animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+0% { transform: rotate(0deg); }
+100% { transform: rotate(360deg); }
+}
+</style>
