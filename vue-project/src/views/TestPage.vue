0ed1358915ed9126be09b6c9e42a01018c70a558
Won Chan Lee <twoone14@tukorea.ac.kr>
2023-08-30 16:50:04 +0900

    test 뷰 추가

---
+++ vue-project/src/views/TestPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="()=>{
+    console.log(this.isLoading)
+  }">click</button>
+    <LoadingSpinner :setloading="true"/>
+</template>
+<script>
+import { mapGetters,mapMutations } from 'vuex';
+import LoadingSpinner from '../components/LoadingSpinner.vue'
+export default {
+  computed: {
+    ...mapGetters(['isLoading'])
+  },
+  methods: {
+    ...mapMutations(['setIsLoading']),
+  },
+  components: {
+    LoadingSpinner,
+  },
+};
+</script>
+<style>
+
+</style>
